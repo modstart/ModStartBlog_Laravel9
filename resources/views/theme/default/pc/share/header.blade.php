abdf9bf44272e869b0663c6487ac5d9846769992
ModStart <modstart@163.com>
2023-01-03 13:26:54 +0800

    feat: ueditor plus update

--- resources/views/theme/default/pc/share/header.blade.php
+++ resources/views/theme/default/pc/share/header.blade.php
@@ -1,12 +1,9 @@
 <header class="ub-header-b">
     <div class="ub-container">
         <div class="menu">
-            @if(\Module\Member\Auth\MemberUser::id())
-                <a href="{{modstart_web_url('member')}}"><i class="iconfont icon-user"></i> {{\Module\Member\Auth\MemberUser::get('username')}}</a>
-            @else
-                <a href="{{modstart_web_url('login')}}">登录</a>
-                @if(!modstart_config('registerDisable',false))
-                    <a href="{{modstart_web_url('register')}}">注册</a>
+            @if(modstart_module_enabled('Member'))
+                @if(\Module\Member\Auth\MemberUser::id())
+                    <a href="{{modstart_web_url('member')}}"><i class="iconfont icon-user"></i> {{\Module\Member\Auth\MemberUser::get('username')}}</a>
                 @endif
             @endif
         </div>
@@ -17,14 +14,7 @@
         </div>
         <div class="nav-mask" onclick="MS.header.hide()"></div>
         <div class="nav">
-            <div class="search">
-                <div class="box">
-                    <form action="{{modstart_web_url('search')}}" method="get">
-                        <input type="text" name="keywords" value="{{empty($keywords)?'':$keywords}}" placeholder="搜索内容"/>
-                        <button type="submit"><i class="iconfont icon-search"></i></button>
-                    </form>
-                </div>
-            </div>
+            @include('module::Vendor.View.searchBox.header')
             {!! \Module\Nav\Render\NavRender::position('head') !!}
         </div>
         <a class="nav-toggle" href="javascript:;" onclick="MS.header.trigger()">
